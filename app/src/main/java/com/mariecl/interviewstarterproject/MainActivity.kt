d09948e80bb03dc57026bc1917c566c90bb396c9
Marie <mariecl@users.noreply.github.com>
2024-06-04 14:54:13 +0100

    Add missing Scaffold

--- app/src/main/java/com/mariecl/interviewstarterproject/MainActivity.kt
+++ app/src/main/java/com/mariecl/interviewstarterproject/MainActivity.kt
@@ -3,7 +3,10 @@ package com.mariecl.interviewstarterproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,10 +18,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             InterviewStarterProjectTheme {
-                Greeting(
-                    name = "Android",
-                    modifier = Modifier.fillMaxWidth()
-                )
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
